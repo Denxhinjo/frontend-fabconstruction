02a3ead0d87ebe6288c256aa48d9e63e2eb13289
Denxhinjo123 <dxhabrahimi2002@gmail.com>
2025-08-05 22:08:51 +0200

    ..

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,7 @@ export default function Footer() {
     <footer className="bg-white shadow-sm mt-auto">
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
         <div className="flex justify-end">
-          <a
-            href="/admin"
-            className="text-sm text-gray-500 hover:text-gray-900"
-          >
-            {t("footer_adminLogin")}
-          </a>
+
         </div>
       </div>
     </footer>
